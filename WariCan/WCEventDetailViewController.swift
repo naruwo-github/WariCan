16fd20d3daa167dd65f86765a98af3c5c21552ef
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2021-04-06 22:41:06 +0900

    レイアウト微調整

--- WariCan/WCEventDetailViewController.swift
+++ WariCan/WCEventDetailViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 import GoogleMobileAds
 
-class WCEventDetailViewController: UIViewController {
+class WCEventDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var bottomBannerView: GADBannerView!
+    @IBOutlet private weak var addPaymentButton: UIButton!
+    @IBOutlet private weak var paymentTableView: UITableView!
+    @IBOutlet private weak var bottomBannerView: GADBannerView!
     
     private let adTestId = "ca-app-pub-3940256099942544/2934735716"
     // TODO: リリースビルドでは、本物の広告IDを使う！
@@ -19,12 +21,31 @@ class WCEventDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupAd()
+        self.setupButtonLayout()
     }
     
     private func setupAd() {
         self.bottomBannerView.adUnitID = adTestId
         self.bottomBannerView.rootViewController = self
         self.bottomBannerView.load(GADRequest())
+    }
+    
+    private func setupButtonLayout() {
+        self.addPaymentButton.layer.cornerRadius = 25
+        self.addPaymentButton.layer.shadowColor = UIColor.black.cgColor
+        self.addPaymentButton.layer.shadowRadius = 4.0
+        self.addPaymentButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+        self.addPaymentButton.layer.shadowOpacity = 0.4
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO:
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO: 
+        return UITableViewCell()
     }
     
 }
