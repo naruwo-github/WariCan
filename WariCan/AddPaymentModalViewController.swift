5c0b8d2de57d941783b61ebeb01a22dcd1ae26fc
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2021-11-04 22:11:09 +0900

    モーダルの土台VC作成

---
+++ WariCan/AddPaymentModalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddPaymentModalViewController.swift
+//  WariCan
+//
+//  Created by Narumi Nogawa on 2021/11/04.
+//
+
+import UIKit
+
+class AddPaymentModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
