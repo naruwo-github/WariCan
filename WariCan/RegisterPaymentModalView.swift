e7a9910481da984b5a0aca0fdd1fd1f990d27b8c
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2021-11-04 21:28:44 +0900

    2つのモーダルViewの土台を抽出

---
+++ WariCan/RegisterPaymentModalView.swift
@@ -0,0 +1,12 @@
+//
+//  RegisterPaymentModalView.swift
+//  WariCan
+//
+//  Created by Narumi Nogawa on 2021/11/04.
+//
+
+import UIKit
+
+// MARK: 支払い追加のモーダルView
+class RegisterPaymentModalView: UIView {
+}
---
+++ WariCan/RegisterPersonModalView.swift
@@ -0,0 +1,12 @@
+//
+//  RegisterPersonModalView.swift
+//  WariCan
+//
+//  Created by Narumi Nogawa on 2021/11/04.
+//
+
+import UIKit
+
+// MARK: 参加者登録のモーダルView
+class RegisterPersonModalView: UIView {
+}
